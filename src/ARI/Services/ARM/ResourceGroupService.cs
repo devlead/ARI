047bc5c100dfa01bc22c3f93296fbf53941f177e
Mattias Karlsson <matkar@wcom.se>
2023-10-14 00:27:27 +0200

    Add missing service

---
+++ src/ARI/Services/ARM/ResourceGroupService.cs
@@ -0,0 +1,21 @@
+﻿namespace ARI.Services.ARM;
+
+public record ResourceGroupService(
+    ILogger<ResourceGroupService> Logger,
+    TokenService TokenService
+    )
+{
+    public async Task<ICollection<ResourceGroup>> GetResourceGroups(string tenantId, string subscriptionId)
+    {
+        var resourceGroups = await TokenService.ARMHttpClientGetAsync<ArmResult<ResourceGroup>>(
+            tenantId,
+            $"https://management.azure.com/subscriptions/{subscriptionId}/resourcegroups?api-version=2021-04-01"
+            );
+
+        ArgumentNullException.ThrowIfNull(resourceGroups.Value);
+
+        return resourceGroups
+                .Value
+                .Index();
+    }
+}
