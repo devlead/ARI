8f33e551bca489fc00260ea238d6adce68d14b81
Mattias Karlsson <matkar@wcom.se>
2023-10-15 23:34:07 +0200

    Fix .gitignore

---
+++ src/ARI.Tests/Unit/Services/ARM/ResourceGroupServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARI.Tests.Unit.Services.ARM;
+
+[TestFixture]
+public class ResourceGroupServiceTests
+{
+    [TestCase(Constants.Tenant.Subscription1.Id)]
+    [TestCase(Constants.Tenant.Subscription2.Id)]
+    public async Task GetSubscriptions(string subscriptionId)
+    {
+        // Given
+        var resourceGroupService = ARIServiceProviderFixture
+                                    .GetRequiredService<ResourceGroupService>();
+
+        // When
+        var result = await resourceGroupService.GetResourceGroups(Constants.Tenant.Id, subscriptionId);
+
+        // Then
+        await Verify(result);
+    }
+}
