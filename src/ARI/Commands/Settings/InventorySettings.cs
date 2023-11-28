f34389c202a7b793a077ce7285d4689d3970e472
Mattias Karlsson <matkar@wcom.se>
2023-11-28 21:18:19 +0100

    Add argument descriptions

--- src/ARI/Commands/Settings/InventorySettings.cs
+++ src/ARI/Commands/Settings/InventorySettings.cs
@@ -15,23 +15,30 @@ public class InventorySettings : CommandSettings
     public DirectoryPath OutputPath { get; set; } = System.Environment.CurrentDirectory;
 
     [CommandOption("--skip-tenant-overview")]
+    [Description("Skip tenant information in overview")]
     public bool SkipTenantOverview { get; set; }
 
     [CommandOption("--run-in-parallel")]
+    [Description("Flag for if generation should be parallized.")]
     public bool RunInParallel { get; set; }
 
     [CommandOption("--markdown-name")]
+    [Description("Markdown file prefix (default \"index\")")]
     public string MarkdownName { get; set; } = "index";
 
     [CommandOption("--skip-frontmatter")]
+    [Description("Skip frontmatter generation")]
     public bool SkipFrontmatter { get; set; }
 
     [CommandOption("--skip-frontmatter-summary")]
+    [Description("Exclude summary from frontmatter")]
     public bool SkipFrontmatterSummary { get; set; }
 
     [CommandOption("--skip-frontmatter-modifed")]
+    [Description("Exclude modfied from frontmatter")]
     public bool SkipFrontmatterModified { get; set; }
 
     [CommandOption("--skip-frontmatter-order")]
+    [Description("Exclude order from frontmatter")]
     public bool SkipFrontmatterOrder { get; set; }
 }
