ae20782e54d60e4b1610b80257573028b7ecb5ba
Mattias Karlsson <matkar@wcom.se>
2024-09-21 21:59:51 +0200

    Update Spectre.Console to 0.49.1

--- src/ARI/Program.cs
+++ src/ARI/Program.cs
@@ -66,7 +66,7 @@ app.Configure(
                 .WithExample(new[] { "inventory", "00000000-0000-0000-0000-000000000000", "outputpath" });
 
         config.SetExceptionHandler(
-            ex => AnsiConsole.WriteException(ex, ExceptionFormats.ShowLinks)
+            (ex , _) => AnsiConsole.WriteException(ex, ExceptionFormats.ShowLinks)
             );
     });
 
